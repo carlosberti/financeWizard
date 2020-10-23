5ac9bcc00d26f211e7fc06ea2fddd166601e8cb8
Carlos Berti <carlosberti182@gmail.com>
2020-10-23 02:31:56 -0300

    feat: create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as s from './styles'
+
+const Header = () => {
+  return (
+    <s.Wrapper>
+      <h1>&#129497; FinanceWizard</h1>
+    </s.Wrapper>
+  )
+}
+
+export default Header
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.header`
+  background: linear-gradient(
+    90deg,
+    rgba(25, 24, 77, 1) 0%,
+    rgba(4, 121, 187, 1) 100%
+  );
+  border-bottom: 0.2rem solid lightgray;
+  padding: 0.8rem;
+  > h1 {
+    color: white;
+  }
+`
