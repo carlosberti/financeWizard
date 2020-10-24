af7a5951a5f32686b7d625ad82fe88f5c3de43c8
Carlos Berti <carlosberti182@gmail.com>
2020-10-24 02:54:37 -0300

    test: change component name

--- src/components/TransactionListItem/test.tsx
+++ src/components/TransactionListItem/test.tsx
@@ -1,24 +1,23 @@
 import { screen, render, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { debug } from 'console'
 
-import ListTransactionItem from '.'
+import TransactionListItem from '.'
 
-describe('<ListTransactionItem />', () => {
+describe('<TransactionListItem />', () => {
   it('should have passed value', () => {
-    render(<ListTransactionItem value="3000" description="qualquer coisa" />)
+    render(<TransactionListItem value="3000" description="qualquer coisa" />)
 
     expect(screen.getByText(/3000/i)).toBeInTheDocument()
   })
 
   it('should not have description if not clicked', () => {
-    render(<ListTransactionItem value="3000" description="qualquer coisa" />)
+    render(<TransactionListItem value="3000" description="qualquer coisa" />)
 
     expect(screen.queryByText(/qualquer coisa/i)).not.toBeInTheDocument()
   })
 
   it('should have description if clicked', async () => {
-    render(<ListTransactionItem value="3000" description="qualquer coisa" />)
+    render(<TransactionListItem value="3000" description="qualquer coisa" />)
 
     const description = screen.getByText(/descrição/i)
 
@@ -30,23 +29,18 @@ describe('<ListTransactionItem />', () => {
   })
 
   it('should have green dollar icon if value is positive', async () => {
-    const { debug } = render(
-      <ListTransactionItem value="3000" description="qualquer coisa" />
-    )
+    render(<TransactionListItem value="3000" description="qualquer coisa" />)
 
     const icon = screen.getByTestId('dollarIcon')
-
-    debug(icon)
 
     expect(icon.getAttribute('color')).toBe('green')
   })
 
   it('should have red dollar icon if value is negative', async () => {
-    render(<ListTransactionItem value="-3000" description="qualquer coisa" />)
+    render(<TransactionListItem value="-3000" description="qualquer coisa" />)
 
     const icon = screen.getByTestId('dollarIcon')
 
-    debug(icon)
     expect(icon.getAttribute('color')).toBe('red')
   })
 })
