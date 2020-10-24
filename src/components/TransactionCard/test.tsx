a3ed8306b2399f78f7fdd3538481db29232107b3
Carlos Berti <carlosberti182@gmail.com>
2020-10-24 02:57:53 -0300

    test: send description onTransaction

--- src/components/TransactionCard/test.tsx
+++ src/components/TransactionCard/test.tsx
@@ -9,7 +9,7 @@ describe('<TransactionCard />', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
-  it('should send input and description values when button is clicked', async () => {
+  it('should send input and description values on successfully transaction', async () => {
     render(<TransactionCard onTransaction={onTransaction} />)
 
     const input = screen.getByRole('textbox', { name: /gasto/i })
@@ -30,7 +30,7 @@ describe('<TransactionCard />', () => {
     })
   })
 
-  it('should not send input if input was filled wrong', async () => {
+  it('should not send values if input was filled wrong', async () => {
     render(<TransactionCard onTransaction={onTransaction} />)
 
     const input = screen.getByRole('textbox', { name: /gasto/i })
@@ -43,7 +43,33 @@ describe('<TransactionCard />', () => {
     })
   })
 
-  it('should reset input value on successfully transaction', async () => {
+  it('should not send values if input was not filled', async () => {
+    render(<TransactionCard onTransaction={onTransaction} />)
+
+    const textArea = screen.getByRole('textbox', { name: /descrição/i })
+    const button = screen.getByRole('button', { name: /fazer transação/i })
+    userEvent.type(textArea, 'qualquer coisa')
+    userEvent.click(button)
+
+    await waitFor(() => {
+      expect(onTransaction).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  it('should not send values if description was not filled', async () => {
+    render(<TransactionCard onTransaction={onTransaction} />)
+
+    const input = screen.getByRole('textbox', { name: /gasto/i })
+    const button = screen.getByRole('button', { name: /fazer transação/i })
+    userEvent.type(input, 'text')
+    userEvent.click(button)
+
+    await waitFor(() => {
+      expect(onTransaction).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  it('should reset input and description value on successfully transaction', async () => {
     render(<TransactionCard onTransaction={onTransaction} />)
 
     const input = screen.getByRole('textbox', { name: /gasto/i })
