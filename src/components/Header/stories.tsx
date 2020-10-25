6df4158eadc65ee9f92d50601460d460c64d28c8
Carlos Berti <carlosberti182@gmail.com>
2020-10-25 00:32:26 -0300

    fix: add balance to header storie

--- src/components/Header/stories.tsx
+++ src/components/Header/stories.tsx
@@ -1,9 +1,12 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Header from '.'
+import Header, { HeaderProps } from '.'
 
 export default {
   title: 'Header',
-  component: Header
+  component: Header,
+  args: {
+    balance: '3878946'
+  }
 } as Meta
 
-export const Default: Story = () => <Header />
+export const Default: Story<HeaderProps> = args => <Header {...args} />
