52f4e5aab78ac280cf0b5d31df6dea6222b0bec8
Carlos Berti <carlosberti182@gmail.com>
2020-10-25 00:16:01 -0300

    feat: create transaction values mock

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,22 +1,22 @@
 export const transactions = [
   {
-    value: '-3000',
-    description: 'primero'
+    value: '-3.000',
+    description: 'compra na padaria'
   },
   {
-    value: '3000.50',
-    description: 'segundo'
+    value: '3.000,50',
+    description: 'salário'
   },
   {
-    value: '3000',
-    description: 'terceiro'
-  },
-  {
-    value: '3000',
-    description: 'quarto'
+    value: '-3.000',
+    description: 'compra de um computador novo'
   },
   {
     value: '3000',
-    description: 'quinto'
+    description: 'pagamento fulano'
+  },
+  {
+    value: '-3.000',
+    description: 'contas do mês'
   }
 ]
