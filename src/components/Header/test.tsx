6d1416ab3cabf55158d22f1441296e6e84f40f45
Carlos Berti <carlosberti182@gmail.com>
2020-10-26 11:24:59 -0300

    test: add context to header

--- src/components/Header/test.tsx
+++ src/components/Header/test.tsx
@@ -1,18 +1,48 @@
 import { render, screen } from '@testing-library/react'
 
 import Header from '.'
+import TransactionsContextProvider, {
+  transactionsContext
+} from 'contexts/transactions'
+import { transactions } from 'mocks'
 
 describe('<Header />', () => {
+  const WrappedHeader = () => (
+    <transactionsContext.Provider value={{ transactions }}>
+      <Header />
+    </transactionsContext.Provider>
+  )
+
   it('should render with heading', () => {
-    render(<Header />)
+    render(<WrappedHeader />)
 
     expect(
       screen.getByRole('heading', { name: /financewizard/i })
     ).toBeInTheDocument()
   })
 
+  it('should render with passed balance', () => {
+    render(<WrappedHeader />)
+
+    expect(
+      screen.getByRole('heading', { name: /saldo: r\$ -2,999\.5/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render with 0 balance if balance is not passed', () => {
+    render(
+      <TransactionsContextProvider>
+        <Header />
+      </TransactionsContextProvider>
+    )
+
+    expect(
+      screen.getByRole('heading', { name: /saldo: r\$ 0,00/i })
+    ).toBeInTheDocument()
+  })
+
   it('should render header with defined background', () => {
-    const { container } = render(<Header />)
+    const { container } = render(<WrappedHeader />)
 
     expect(container.firstChild).toHaveStyle({
       backgroundColor: 'rgb(102, 111, 139)'
